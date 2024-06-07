01659791e1ab4dbc2f781c2e17ad00a7634878c4
Almiron Augusto <almironaugusto404@gmail.com>
2024-06-07 19:57:40 -0300

    react_commit

---
+++ primer_proyecto_react/src/assets/App.jsx
@@ -0,0 +1,21 @@
+export function App (){
+    return (
+            <article>
+                <header>
+                    <img src="https://unavatar.io/midudev.com" alt="imagen" />
+                    <div>
+                        <h1>Mi dudev</h1>
+                        <strong>Miguel Angel Duran</strong>
+                        <span>@midudev</span>
+                    </div>
+                    <aside>
+                        <button>
+                            Seguir
+                        </button>
+                    </aside>
+                </header> 
+            </article>
+    
+
+    ) 
+}
